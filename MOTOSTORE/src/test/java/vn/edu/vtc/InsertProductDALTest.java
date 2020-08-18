49f4aac532b54b4ac1b211dcfe31533dfab2253f
unknown <duoclv.260500.halong@gmail.com>
2020-08-18 19:51:08 +0700

    test insertProductDAL

---
+++ MOTOSTORE/src/test/java/vn/edu/vtc/InsertProductDALTest.java
@@ -0,0 +1,74 @@
+package vn.edu.vtc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import vn.edu.vtc.dal.ProductDAL;
+import vn.edu.vtc.persitance.Product;
+
+public class InsertProductDALTest {
+    @Test
+    public void insertProductDALTest1(){
+        ProductDAL productDAL = new ProductDAL();
+        Product product = new Product();
+        product.setProductName("productName1"); //name is unique
+        product.setDescription("description1. description2. description3.");
+        product.setPrice(100000000); //Price haven to numbers
+        product.setSize("length x width x height 200 x 50 x 90");
+        product.setColor("productName");
+        product.setTimeWarranty("productName");
+        product.setCategoryID(1); //ID 1-4
+        int result = productDAL.insertProductDAL(product);
+        int expected = 1;
+        assertTrue(result==expected);
+    }
+
+    @Test
+    public void insertProductDALTest2(){
+        ProductDAL productDAL = new ProductDAL();
+        Product product = new Product();
+        product.setProductName("productName1"); //Name not unique
+        product.setDescription("description1. description2. description3.");
+        product.setPrice(1000000000);
+        product.setSize("length x width x height 200 x 50 x 90");
+        product.setColor("productName");
+        product.setTimeWarranty("productName");
+        product.setCategoryID(1);
+        int result = productDAL.insertProductDAL(product);
+        int expected = 0;
+        assertTrue(result==expected);
+    }
+
+    @Test
+    public void insertProductDALTest3(){
+        ProductDAL productDAL = new ProductDAL();
+        Product product = new Product();
+        product.setProductName("productName3");
+        product.setDescription("description1. description2. description3.");
+        product.setPrice(-1000000000); //Price not greater than 0
+        product.setSize("length x width x height 200 x 50 x 90");
+        product.setColor("productName");
+        product.setTimeWarranty("productName");
+        product.setCategoryID(1);
+        int result = productDAL.insertProductDAL(product);
+        int expected = 0;
+        assertTrue(result==expected);
+    }
+
+    @Test
+    public void insertProductDALTest4(){
+        ProductDAL productDAL = new ProductDAL();
+        Product product = new Product();
+        product.setProductName("productName3");
+        product.setDescription("description1. description2. description3.");
+        product.setPrice(1000000000);
+        product.setSize("length x width x height 200 x 50 x 90");
+        product.setColor("productName");
+        product.setTimeWarranty("productName");
+        product.setCategoryID(5); //ID not in the range 1-4
+        int result = productDAL.insertProductDAL(product);
+        int expected = 0;
+        assertTrue(result==expected);
+    }
+}
