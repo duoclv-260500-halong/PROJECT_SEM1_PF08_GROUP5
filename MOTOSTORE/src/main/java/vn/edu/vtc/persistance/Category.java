5566ca8d7116736a30856a2edb647b2374df99e9
unknown <duoclv.260500.halong@gmail.com>
2020-08-24 17:20:47 +0700

    category persistance

---
+++ MOTOSTORE/src/main/java/vn/edu/vtc/persistance/Category.java
@@ -0,0 +1,13 @@
+package vn.edu.vtc.persistance;
+
+public class Category {
+    private String categoryID;
+
+    public String getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(String categoryID) {
+        this.categoryID = categoryID;
+    }
+}
