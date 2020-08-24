c8d041459217ed37ed1c47e4449266de5ba59361
unknown <duoclv.260500.halong@gmail.com>
2020-08-24 17:16:21 +0700

    App

--- MOTOSTORE/src/main/java/vn/edu/vtc/App.java
+++ MOTOSTORE/src/main/java/vn/edu/vtc/App.java
@@ -1,11 +1,14 @@
 package vn.edu.vtc;
+
+import vn.edu.vtc.bl.CustomerBL;
 import vn.edu.vtc.bl.OrderBL;
+import vn.edu.vtc.bl.OrderDetailsBL;
 import vn.edu.vtc.bl.ProductBL;
 import vn.edu.vtc.bl.UserBL;
-import vn.edu.vtc.persitance.Customer;
-import vn.edu.vtc.persitance.Order;
-import vn.edu.vtc.persitance.Product;
-import vn.edu.vtc.persitance.User;
+import vn.edu.vtc.persistance.Customer;
+import vn.edu.vtc.persistance.Order;
+import vn.edu.vtc.persistance.Product;
+import vn.edu.vtc.persistance.User;
 
 import java.io.Console;
 import java.sql.SQLException;
@@ -31,6 +34,13 @@ public class App {
         menuInsert.add("3. Insert Clothes");
         menuInsert.add("4. Insert Gloves");
         menuInsert.add("0. Exit");
+
+        ArrayList<String> menuUpdateOrder = new ArrayList<String>();
+        menuUpdateOrder.add("Menu Update Order");
+        menuUpdateOrder.add("1. Update Customer");
+        menuUpdateOrder.add("2. Update Product");
+        menuUpdateOrder.add("0. Exit");
+
         checkLogin();
 
         int choice = printMenu(mainMenu, 4);
@@ -45,12 +55,12 @@ public class App {
                 break;
             }
             case 3: {
-
                 createOrder();
                 break;
             }
             case 4: {
-                
+                updateOrder();
+                break;
             }
         }
     }
@@ -85,11 +95,10 @@ public class App {
             Console console = System.console();
             char[] passwordArray = console.readPassword("Input password: ");
             String password = new String(passwordArray);
-            if(password.equals("")){
+            if (password.equals("")) {
                 System.out.println("Password not correct");
                 continue;
             }
-            
             user = userBL.getUser(username, password);
             if (user != null) {
                 System.out.println("Login successfully");
@@ -101,13 +110,6 @@ public class App {
         return user;
     }
 
-    
-    // public static void getByName(String nameProduct){
-    //     ProductBL productBL = new ProductBL();
-    //     Scanner scanner = new Scanner(System.in);
-    //     System.out.print("Name Product: ");
-    //     nameProduct = scanner.nextLine();
-    // }
     public static void insertProduct(int choice1) {
         Product product = new Product();
         ProductBL productBL = new ProductBL();
@@ -143,7 +145,7 @@ public class App {
             System.out.print("Time Warranty: ");
             product.setTimeWarranty(scanner.nextLine());
             if (productBL.insertProductBL(product)) {
-                System.out.println("Insert completed!"); 
+                System.out.println("Insert completed!");
             } else {
                 System.out.println("Insert failed!");
             }
@@ -152,17 +154,15 @@ public class App {
             System.out.println("Price just have numbers");
             insertProduct(choice1);
         }
-        
 
     }
-    
+
     public static void updateProduct() {
         Product product = new Product();
         ProductBL productBL = new ProductBL();
         while (true) {
-            System.out.print("Nhap san pham can tim: ");
+            System.out.print("Input product name need to update: ");
             String productName = scanner.nextLine();
-            
             product = productBL.getByName(productName);
             if (product != null) {
                 try {
@@ -197,32 +197,32 @@ public class App {
     public static void createOrder() {
         Order order = new Order();
         Customer customer = new Customer();
-        //insert customer
-        customer = insertCustomer();
+        // insert customer
+        customer = insertCustomerToOrder();
         order.setCustomer(customer);
-        //insert products
-        do{
+        // insert products
+        do {
             order.addProduct(insertProductToOrder());
             System.out.print("Do you want to add another products? (Yes/No): ");
             String confirm = scanner.nextLine();
-            if(confirm.equalsIgnoreCase("no")){
+            if (confirm.equalsIgnoreCase("no")) {
                 break;
             }
-        }while(true);
+        } while (true);
         OrderBL orderBL = new OrderBL();
-        if(orderBL.insertOrder(order)){
+        if (orderBL.insertOrder(order)) {
             System.out.println("Insert completed");
-        }
-        else{
+            System.out.println("Your order");
+            
+        } else {
             System.out.println("Insert failed");
 
         }
 
     }
 
-    public static Customer insertCustomer(){
+    public static Customer insertCustomerToOrder() {
         Customer customer = new Customer();
-        
         System.out.print("Name Customer: ");
         customer.setCustomerName(scanner.nextLine());
         System.out.print("Address Customer: ");
@@ -231,33 +231,143 @@ public class App {
         customer.setPhoneNumber(scanner.nextLine());
         System.out.print("Identity Card: ");
         customer.setIdentityCard(scanner.nextLine());
-    return customer;
+        return customer;
     }
 
-    public static Product insertProductToOrder(){
+    public static Product insertProductToOrder() {
         System.out.print("Name Product: ");
         String productName = scanner.nextLine();
         Product product = getByName(productName);
-        if(product != null){
-            System.out.print("Input quantity: ");
-            int quantity = Integer.parseInt(scanner.nextLine());        
-            product.setQuantity(quantity);
+        if (product != null) {
+            
+            do{
+                System.out.print("Input quantity: ");
+                try {
+                    int quantity = Integer.parseInt(scanner.nextLine());
+                product.setQuantity(quantity);
+                break;
+                } catch (Exception e) {
+                    System.out.println("Quantity is very large!");
+                    
+                }
+            }while(true);
+            
+            
         }
         return product;
     }
 
-    public static Product getByName(String productName){
+    public static Product getByName(String productName) {
         Product product = new Product();
         ProductBL productBL = new ProductBL();
         product = productBL.getByName(productName);
-        if(product != null){
+        if (product != null) {
             System.out.println("Product found");
             return product;
-        }
-        else{
+        } else {
             System.out.println("Product not found");
             return null;
         }
     }
-    
+
+    public static void updateOrder(){
+        Order order = getOrderByID();
+        System.out.println("Update Order");
+        System.out.println("1. Customer");
+        System.out.println("2. List Products");
+        System.out.print("Update?(1/2): ");
+        int choice = Integer.valueOf(scanner.nextLine());
+        switch(choice){
+            case 1:{
+                updateCustomer();
+                break;
+            }
+            case 2:{
+                insertProductToUpdateOrder(order.getOrderID());
+                break;
+            }
+        }
+    }
+
+    public static Order getOrderByID(){
+        Order order = new Order();
+        OrderBL orderBL = new OrderBL();
+        do{
+            try {
+                System.out.print("Input orderID: ");
+            int orderID = Integer.valueOf(scanner.nextLine());
+            order = orderBL.getByID(orderID);
+            if(order.getOrderID() == orderID){
+                break;
+            }
+            else{
+                System.out.print("Order not found!\nDo you want to continue?(Yes/No): ");
+                String confirm = scanner.nextLine();
+                if(confirm.equalsIgnoreCase("NO")){
+                    System.exit(0);
+                }
+            }
+            } catch (Exception e) {
+                System.out.print("Order not found!\nDo you want to continue?(Yes/No): ");
+                String confirm = scanner.nextLine();
+                if(confirm.equalsIgnoreCase("NO")){
+                    System.exit(0);
+                }
+            }
+        }while(true);
+        return order;
+    }
+    public static void updateCustomer() {
+        Customer customer = new Customer();
+        CustomerBL customerBL = new CustomerBL();
+        while (true) {
+            System.out.print("Input customerID: ");
+            String customerID = scanner.nextLine();
+            customer = customerBL.getCustomer(customerID);
+            if (customer != null) {
+                try {
+                    System.out.print("Name Customer: ");
+                    customer.setCustomerName(scanner.nextLine());
+                    System.out.print("Address Customer: ");
+                    customer.setCustomerAddress(scanner.nextLine());
+                    System.out.print("Phone Number: ");
+                    customer.setPhoneNumber(scanner.nextLine());
+                    System.out.print("Identity Card: ");
+                    customer.setIdentityCard(scanner.nextLine());
+                    if (customerBL.updateCustomer(customer, customerID)){
+                        System.out.println("Update completed!");
+                        break;
+                    } else {
+                        System.out.println("Update failed!");
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Update failed");
+                }
+
+            } else {
+                System.out.print("Customer not found \n");
+            }
+        }
+    }
+
+    public static void insertProductToUpdateOrder(int orderID){
+        Order order = new Order();
+        order.setOrderID(orderID);
+        OrderDetailsBL orderDetailsBL = new OrderDetailsBL();
+        do {
+            order.addProduct(insertProductToOrder());
+            System.out.print("Do you want to add another products? (Yes/No): ");
+            String confirm = scanner.nextLine();
+            if (confirm.equalsIgnoreCase("no")) {
+                break;
+            }
+        } while (true);
+        if (orderDetailsBL.updateOrderDetails(order, orderID)) {
+            System.out.println("Update completed");
+        }
+        else {
+            System.out.println("Update failed");
+        }
+    }
 }
