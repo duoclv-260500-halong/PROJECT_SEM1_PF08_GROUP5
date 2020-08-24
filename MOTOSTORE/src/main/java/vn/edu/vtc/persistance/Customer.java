f52a672fe22912c8bd166aed8a91d3efd99c3365
unknown <duoclv.260500.halong@gmail.com>
2020-08-24 17:21:01 +0700

    customer persistance

---
+++ MOTOSTORE/src/main/java/vn/edu/vtc/persistance/Customer.java
@@ -0,0 +1,50 @@
+package vn.edu.vtc.persistance;
+
+public class Customer {
+    private int customerID;
+    private String customerName;
+    private String customerAddress;
+    private String phoneNumber;
+    private String identityCard;
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIdentityCard() {
+        return identityCard;
+    }
+
+    public void setIdentityCard(String identityCard) {
+        this.identityCard = identityCard;
+    }
+    
+}
