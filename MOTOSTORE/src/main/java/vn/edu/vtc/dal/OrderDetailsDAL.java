33a19962257b36bc4579872d5ba2a1f47ce6180f
unknown <duoclv.260500.halong@gmail.com>
2020-08-24 17:19:18 +0700

    orderDetailsDAL

---
+++ MOTOSTORE/src/main/java/vn/edu/vtc/dal/OrderDetailsDAL.java
@@ -0,0 +1,51 @@
+package vn.edu.vtc.dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+import vn.edu.vtc.persistance.Order;
+import vn.edu.vtc.persistance.Product;
+
+
+public class OrderDetailsDAL {
+    
+    public int updateOrderDetails(Order order, int orderID){
+        try (Connection connection = DBUtil.getConnection();){
+            connection.setAutoCommit(false);
+            //delete OrderDetails
+            try (PreparedStatement pstm = connection.prepareStatement("delete from OrderDetails where orderID = ?")){
+                pstm.setInt(1, orderID);
+                if(pstm.executeUpdate() <= 0){
+                    throw new SQLException();
+                }
+            } catch (Exception e) {
+                connection.rollback();
+            }
+            //insert OrderDetails
+            for (Product product : order.getProducts()) {
+                try (PreparedStatement pstm = connection.prepareStatement("insert into OrderDetails(orderID, productName, unitPrice, quantity) values (?,?,?,?)");){
+                    pstm.setInt(1, order.getOrderID());
+                    pstm.setString(2, product.getProductName());
+                    pstm.setLong(3, product.getPrice());
+                    pstm.setInt(4, product.getQuantity());
+                    if(pstm.executeUpdate() <= 0){
+                        throw new SQLException();
+                    }                
+                } catch (Exception e) {
+                    connection.rollback();
+                    return 0;
+                }
+            }
+            connection.commit();
+            connection.setAutoCommit(true);
+            
+        } catch (Exception e) {
+            return 0;
+        }
+        return 1;
+    }
+    
+}
