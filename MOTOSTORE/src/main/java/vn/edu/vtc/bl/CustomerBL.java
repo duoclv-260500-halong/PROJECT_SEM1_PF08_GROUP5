6f9b03cccb2bc732377dbc91a823229f4304987a
unknown <duoclv.260500.halong@gmail.com>
2020-08-26 19:07:41 +0700

    customerBL

--- MOTOSTORE/src/main/java/vn/edu/vtc/bl/CustomerBL.java
+++ MOTOSTORE/src/main/java/vn/edu/vtc/bl/CustomerBL.java
@@ -6,11 +6,11 @@ import vn.edu.vtc.persistance.Customer;
 public class CustomerBL{
     CustomerDAL customerDAL = new CustomerDAL();
 
-    public Customer getCustomer(String customerID){
+    public Customer getCustomer(int customerID){
         return customerDAL.getCustomer(customerID);
     }
 
-    public boolean updateCustomer(Customer customer, String customerID){
+    public boolean updateCustomer(Customer customer, int customerID){
         return customerDAL.updateCustomer(customer, customerID) > 0;
     }
 }
