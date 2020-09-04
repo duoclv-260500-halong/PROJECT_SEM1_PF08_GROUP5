f156e2d467c9e4abebc1db5712e9c359c7817f44
unknown <duoclv.260500.halong@gmail.com>
2020-09-05 02:16:36 +0700

    validation

--- MOTOSTORE/src/main/java/vn/edu/vtc/bl/Validation.java
+++ MOTOSTORE/src/main/java/vn/edu/vtc/bl/Validation.java
@@ -13,4 +13,13 @@ public class Validation {
         Matcher m = p.matcher(password);
         return m.matches();
     }  
+    public boolean isValidPhoneNumber(String phoneNumber){
+        String regex = "^(?=.*[0-9]).{10}$";
+        Pattern p = Pattern.compile(regex); 
+        if (phoneNumber == null) { 
+            return false;
+        }
+        Matcher m = p.matcher(phoneNumber);
+        return m.matches();
+    }
 }
